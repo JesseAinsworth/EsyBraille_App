160b5c092cc7fa737ce9229c3ceddbef04b3fecf
IsaacValenciaZ <al222210520@gmail.com>
2025-11-12 18:53:13 -0600

    actualizacion de build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,86 +1,172 @@
 plugins {
+
     alias(libs.plugins.android.application)
+
     alias(libs.plugins.kotlin.android)
+
     alias(libs.plugins.kotlin.compose)
+
     id("org.sonarqube") version "5.1.0.4882"
+
 }
+
 sonar {
+
     properties {
+
         property("sonar.projectKey", "JesseAinsworth_EsyBraille_App_f8738890-426e-40e8-9768-f722f1d0249a")
+
         property("sonar.projectName", "EsyBraille_App")
+
     }
+
 }
+
+
 
 android {
+
     namespace = "com.example.esybrailleapp"
+
     compileSdk = 36
 
+
+
     defaultConfig {
+
         applicationId = "com.example.esybrailleapp"
+
         minSdk = 23
+
         targetSdk = 36
+
         versionCode = 1
+
         versionName = "1.0"
 
+
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
+
+
     buildTypes {
+
         release {
+
             isMinifyEnabled = false
+
             proguardFiles(
+
                 getDefaultProguardFile("proguard-android-optimize.txt"),
+
                 "proguard-rules.pro"
+
             )
+
         }
+
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+
+        sourceCompatibility = JavaVersion.VERSION_17
+
+        targetCompatibility = JavaVersion.VERSION_17
+
     }
+
     kotlinOptions {
-        jvmTarget = "11"
+
+        jvmTarget = "17"
+
     }
+
     buildFeatures {
+
         compose = true
+
     }
+
+
+
 
 
     packaging {
+
         resources {
+
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+
             excludes += "META-INF/INDEX.LIST"
+
             excludes += "META-INF/DEPENDENCIES"
+
             excludes += "META-INF/io.netty.versions.properties"
+
         }
+
     }
+
 }
+
+
 
 dependencies {
 
+
+
     implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.compose.ui)
+
     implementation(libs.androidx.compose.ui.graphics)
+
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     implementation(libs.androidx.compose.material3)
+
     implementation("androidx.core:core-splashscreen:1.0.1")
+
     implementation("androidx.navigation:navigation-compose:2.7.1")
+
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
     implementation("com.google.mlkit:text-recognition:16.0.0")
+
     implementation(libs.firebase.appdistribution.gradle)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
+
     androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
+
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
+
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
 
+
+
+
 }
+
